187a65459127900a739b3795948b8a9d6a87c3c8
Tiziano Cavalieri <tizcav99@gmail.com>
2024-08-22 10:59:13 +0100

    added a docstring to df_dict_to_xlsx

--- tools/df_dict_to_xlsx.py
+++ tools/df_dict_to_xlsx.py
@@ -1,6 +1,11 @@
 import pandas as pd
 from openpyxl.workbook import Workbook 
 def df_dict_to_xlsx(dict):
+    """
+    This function write the pandas dataframes in a dictionary to a .xlsx file.
+    parameters:
+    dict (dict): dictionary of dataframes.
+    """   
     for key in dict.keys():
         df = dict[key]
         file = key + '.xlsx'
